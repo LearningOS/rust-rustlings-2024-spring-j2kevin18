e9e9af9464393544508b81ea5de71db87d22cff9
j2kevin18 <70083630+j2kevin18@users.noreply.github.com>
2024-04-10 13:46:01 +0000

    update

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -34,16 +34,11 @@
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let timestamp = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-
-    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
-    println!("cargo:rustc-cfg=feature=\"pass\"");
+    // println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
+    // println!("cargo:rustc-cfg=feature=\"pass\"");
 }
 
 #[cfg(test)]
