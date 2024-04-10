5ff972037626f63cf41913c5d8a36c663d06e6a6
j2kevin18 <70083630+j2kevin18@users.noreply.github.com>
2024-04-10 12:32:13 +0000

    update

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -36,7 +36,15 @@
 
 // I AM NOT DONE
 
-fn main() {}
+fn main() {
+    let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
+    println!("cargo:rustc-cfg=feature=\"pass\"");
+}
 
 #[cfg(test)]
 mod tests {
